213aaa65f5623c7b0f81c5356e1f8944c928d6ca
Sarans13 <114057996+Sarans13@users.noreply.github.com>
2023-03-31 14:55:07 +0530

    Some changes in industry and ngo section

--- public/js/ngo.js
+++ public/js/ngo.js
@@ -57,7 +57,7 @@ const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
 	const itemToggle = this.getAttribute("aria-expanded");
-
+	let i;
 	for (i = 0; i < items.length; i++) {
 		items[i].setAttribute("aria-expanded", "false");
 	}
